3ad622fe731fad0ff5e2aff894f6672b180c8091
isara <issarapichairat@gmail.com>
2026-06-12 23:36:40 +0700

    first commit

---
+++ src/config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  NODE_ENV: process.env.NODE_ENV,
+  port: parseInt(process.env.PORT),
+  secret: process.env.SECRET,
+  dbHost: process.env.DB_HOST,
+  dbPort: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+});
